3ec7ecce4a21f48eb8b6365b6f49a0b74a3e819b
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-11-16 22:54:08 +0300

    16 Nov 2024

--- #src/js/main.js
+++ #src/js/main.js
@@ -7,7 +7,15 @@ import dynamicAdaptive from './libraries/move-elements.js';
 dynamicAdaptive();
 
 //* ----------------------------------------------------------------------------
+import Waves from './animations/waves.jsx';
 
+document.addEventListener('DOMContentLoaded', () => {
+	const waves = new Waves('#holder', {
+		waves: 3,
+		width: 300,
+	});
+	waves.animate();
+});
 //* ----------------------------------------------------------------------------
 console.log('%c РОССИЯ ',
 	'background: blue; color: yellow; font-size: x-large; border-left: 5px solid black; border-top: 30px solid white; border-right: 2px solid black; border-bottom: 30px solid red;');
