02427a158e6e2d07eda083b67c65242347decc6c
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-11-25 22:15:35 +0300

    25 Nov 2024

--- #src/js/main.js
+++ #src/js/main.js
@@ -16,25 +16,7 @@ loaded('.preloader');
 // 		waves.animate();
 // 	});
 // }
-//* ----------------------------------------------------------------------------
-const isYaBrowser = /YaBrowser/i.test(navigator.userAgent);  // Проверка на Яндекс.Браузер
-const isMobile = /Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);  // Проверка на мобильное устройство
-const isYaBrowserMobile = isYaBrowser && /Mobile/i.test(navigator.userAgent);  // Проверка на мобильный Яндекс.Браузер
 
-if (isYaBrowser || isYaBrowserMobile) {
-	console.log(isYaBrowserMobile ? 'Это мобильный Яндекс.Браузер' : 'Это ПК Яндекс.Браузер');
-	const button = document.querySelector('.page__aside');
-	if (button) {
-		button.style.display = 'none'; // Скрыть кнопку
-	}
-} else if (isMobile) {
-	console.log('Это мобильное устройство, но не Яндекс.Браузер');
-	// Дополнительные действия для мобильных устройств, не являющихся Яндекс.Браузером
-} else {
-	console.log('Это не Яндекс.Браузер');
-}
-
-console.log(navigator.userAgent);
 //* ----------------------------------------------------------------------------
 console.log('%c РОССИЯ ',
 	'background: blue; color: yellow; font-size: x-large; border-left: 5px solid black; border-top: 30px solid white; border-right: 2px solid black; border-bottom: 30px solid red;');
