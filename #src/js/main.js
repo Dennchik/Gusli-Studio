50ba40038b81d830bc04983843ef417e64504dc0
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-11-25 20:38:04 +0300

    25 Nov 2024

--- #src/js/main.js
+++ #src/js/main.js
@@ -16,8 +16,11 @@ loaded('.preloader');
 // 		waves.animate();
 // 	});
 // }
-
-
+//* ----------------------------------------------------------------------------
+document.body.addEventListener('touchmove', function (event) {
+	event.preventDefault();
+	event.stopPropagation();
+}, false);
 //* ----------------------------------------------------------------------------
 console.log('%c РОССИЯ ',
 	'background: blue; color: yellow; font-size: x-large; border-left: 5px solid black; border-top: 30px solid white; border-right: 2px solid black; border-bottom: 30px solid red;');
