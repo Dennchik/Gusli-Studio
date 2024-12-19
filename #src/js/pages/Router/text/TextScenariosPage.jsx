a2e23a64260579998bf56ef98f5ba6753d4ac5e0
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-12-20 01:56:33 +0300

    Test-01

--- #src/js/pages/Router/text/TextScenariosPage.jsx
+++ #src/js/pages/Router/text/TextScenariosPage.jsx
@@ -4,43 +4,42 @@ import { useGSAP } from '@gsap/react';
 import { ScrollSmoother } from 'gsap/ScrollSmoother';
 
 import returnToSavedPosition from '../../../modules/return-position.js';
-// import { applyParallax } from '../../../animations/animations.jsx';
+import { applyParallax } from '../../../animations/animations.jsx';
 
 import { Header } from '../../../components/layouts/Header.jsx';
+import { ServiceText } from '../../../components/categories/ServiceText.jsx';
 import { Footer } from '../../../components/layouts/Footer.jsx';
-import { Offer } from '../../../components/chunks/Offer.jsx';
 import { MenuFloat } from '../../../components/layouts/Menu-float.jsx';
 import { FormModal } from '../../../components/layouts/FormModal.jsx';
-// import {
-// 	SectionTextScenarios
-// } from '../../../components/categories/text/SectionTextScenarios.jsx';
 
 gsap.registerPlugin(useGSAP, ScrollSmoother);
-const baseUrl = '../..';
+const baseUrl = '..';
 
-function TextScenariosPage() {
-	// const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+function TextPage() {
+	const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
 	useGSAP(
 		() => {
 			// create the smooth scroller FIRST!
-			const smoother = ScrollSmoother.create({
-				wrapper: '#wrapper',
-				content: '#content',
-				smooth: 1.5,
-				effects: true,
-				smoothTouch: 0.1,
-			});
-			return () => {
-				smoother.kill(); // Удаляем Smooth при размонтировании
-			};
+			if (!isMobile) {
+				const smoother = ScrollSmoother.create({
+					wrapper: '#wrapper',
+					content: '#content',
+					smooth: 1.5,
+					effects: true,
+					smoothTouch: 0.1,
+				});
+				return () => {
+					smoother.kill(); // Удаляем Smooth при размонтировании
+				};
+			}
 		},
 	);
 
-
 	useEffect(() => {
-		// if (!isMobile) {
-		// 	applyParallax('.material-parallax');
-		// }
+		if (!isMobile) {
+			applyParallax('.material-parallax');
+		}
 		returnToSavedPosition();
 	}, []);
 
@@ -52,11 +51,8 @@ function TextScenariosPage() {
 			<main className="page__main-content">
 				<div className="main-content" id="wrapper">
 					<div className="main-content__content" id="content">
-						<section className="main-content__body">
-							{/* <SectionTextScenarios baseUrl={baseUrl} isHomePage={true} /> */}
-						</section>
-						<section className="main-content__offer gradient-neon-color">
-							<Offer baseUrl={baseUrl} />
+						<section className="main-content__services">
+							<ServiceText baseUrl={baseUrl} />
 						</section>
 						<footer className="main-content__footer" id="footer">
 							<Footer baseUrl={baseUrl} isHomePage={true} />
@@ -64,18 +60,17 @@ function TextScenariosPage() {
 					</div>
 				</div>
 			</main>
-
 			<div className="page__menu-float">
 				<MenuFloat baseUrl={baseUrl} />
 			</div>
 			<div className="page__aside" id="scrollButton">
 				<i className="icon-angle-down _button"></i>
 			</div>
-			<div className="page__form-modal">
+			<section className="page__form-modal">
 				<FormModal />
-			</div>
+			</section>
 		</>
 	);
 }
 
-export default TextScenariosPage;
+export default TextPage;
